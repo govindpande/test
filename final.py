82376f47d5da95d8632413becaad85b0285f9d49
Govind Pande <117948813+govindpande@users.noreply.github.com>
2023-05-17 10:31:38 -0400

    Update final.py

--- final.py
+++ final.py
@@ -3,12 +3,6 @@ import streamlit as st
 #import altair as alt
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
-import yfinance as yf
-import plotly.graph_objs as go
-from prophet import Prophet
-import ydata_profiling
-from ydata_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
 
 st.sidebar.markdown("**Author**: Govind Pande")
 
